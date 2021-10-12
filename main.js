d776ddeba1bbf26fd2029d1f0cb021f877159844
Arya390 <75195330+Arya390@users.noreply.github.com>
2021-10-12 15:24:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,28 @@ function setup() {
 	canvas.parent('canvas');
 	
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX +", noseY = " + noseY);
+	}
 }
 
 function draw() {
